e84223dce69438a46d7b42c84fe94af2c2f7ca04
Sawan Sancheti <sawan.sancheti@nagarro.com>
2020-04-12 07:33:07 +0530

    second commit

--- src/LevelTwoAlgorithms/Backpropagation.java
+++ src/LevelTwoAlgorithms/Backpropagation.java
@@ -4,11 +4,11 @@ import Utility.Weights;
 
 public class Backpropagation {
 	
-	Weights weights;
+	public Weights weights = new Weights();
 	
-	void updateWeights(float fnameMatch, float lnameMatch, float addressMatch, float target) {
-		weights.fname += weights.alpha * target * 0.5 * Math.pow((double)(target = fnameMatch), 2);
-		weights.lname += weights.alpha * target * 0.5 * Math.pow((double)(target = lnameMatch), 2);
-		weights.address += weights.alpha * target * 0.5 * Math.pow((double)(target = addressMatch), 2);
+	public void updateWeights(float fnameMatch, float lnameMatch, float addressMatch, float target) {
+		weights.fname += weights.alpha * target * 0.5f * (float)Math.pow((double)(target - fnameMatch), 2);
+		weights.lname += weights.alpha * target * 0.5f * (float)Math.pow((double)(target - lnameMatch), 2);
+		weights.address += weights.alpha * target * 0.5f * (float)Math.pow((double)(target - addressMatch), 2);
 	}
 }
--- src/Utility/Weights.java
+++ src/Utility/Weights.java
@@ -1,7 +1,7 @@
 package Utility;
 
 public class Weights {
-	public float fname = 0.0f, lname = 0.0f, address = 0.0f, alpha = 0.0f;
+	public float fname = 0.5f, lname = 0.5f, address = 0.5f, alpha = 0.5f;
 	
 	public String toString() {
 		return "\nfname = " + fname + "\nlname = " + lname + "\naddress = " + address;
