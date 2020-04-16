3fd554c88e87a26b52f92ca2984d55f461bef4b2
Sawan Sancheti <sawan.sancheti@nagarro.com>
2020-04-16 16:59:23 +0530

    GradientDescentUpdated

--- src/LevelTwoAlgorithms/GradientDescent.java
+++ src/LevelTwoAlgorithms/GradientDescent.java
@@ -7,8 +7,8 @@ public class GradientDescent {
 	public Weights weights = new Weights();
 	
 	public void updateWeights(float fnameMatch, float lnameMatch, float addressMatch, float target, int sample) {
-		weights.fname += weights.alpha * target * (float)(1 / sample) * (float)Math.pow((double)(target - (weights.fname + fnameMatch) / 2), 2);
-		weights.lname += weights.alpha * target * (float)(1 / sample) * (float)Math.pow((double)(target - (weights.lname + lnameMatch) / 2), 2);
-		weights.address += weights.alpha * target * (float)(1 / sample) * (float)Math.pow((double)(target - (weights.address + addressMatch) / 2), 2);
-	}
+	        weights.fname += weights.alpha * target * (float)(1.0 /(float) sample) * Math.pow((double)(target - fnameMatch), 2);
+	        weights.lname += weights.alpha * target * (float)(1.0 /(float) sample) * Math.pow((double)(target - lnameMatch), 2);
+	        weights.address += weights.alpha * target * (float)(1.0 /(float) sample) * Math.pow((double)(target - addressMatch), 2);
+	    }
 }
